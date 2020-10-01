3e85705275e106d35d222042985171a6ab1d851f
VicenteLucero <vjlucero@miuandes.cl>
2020-10-01 17:35:43 -0300

    login - signin fixed

---
+++ Assets/Code/UserClass.cs
@@ -0,0 +1,20 @@
+﻿public class User
+{
+    public string username;
+    public string email;
+    public string password;
+    public int wins;
+    public int loses;
+
+    public User()
+    {
+    }
+
+    public User(string username, string email, string password, int wins, int loses)
+    {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+
+    }
+}
--- Assets/Code/UserLogin.cs
+++ Assets/Code/UserLogin.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 using Firebase.Auth;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -9,14 +12,23 @@ using System;
 public class UserLogin : MonoBehaviour
 {
     private FirebaseAuth auth;
+    private DatabaseReference reference;
+    private FirebaseDatabase dbInstance;
     public InputField UserNameInput, PasswordInput, RePasswordInput, EmailInput;
     public Button SignupButton, LoginButton, CreateButton, BackButton;
     public Text ErrorText, RePassWordLabel, EmailLabel;
 
+
     void Start()
     {
+
         Debug.Log("started");
         auth = FirebaseAuth.DefaultInstance;
+
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://zeld-e907d.firebaseio.com/");
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+        dbInstance = FirebaseDatabase.DefaultInstance;
+
         CreateButton.gameObject.SetActive(false);
         BackButton.gameObject.SetActive(false);
         RePasswordInput.gameObject.SetActive(false);
@@ -53,41 +65,56 @@ public class UserLogin : MonoBehaviour
         EmailInput.gameObject.SetActive(false);
         SignupButton.gameObject.SetActive(true);
         LoginButton.gameObject.SetActive(true);
+        ErrorText.text = "";
     }
 
     public void Signup(string username, string password, string re_password, string email)
     {
         if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(re_password) || string.IsNullOrEmpty(email))
         {
-            //Error handling
+            ErrorText.text = "Empty Values";
             return;
         }
 
         if (password != re_password)
         {
+            ErrorText.text = "Passwords do not match";
             return;
         }
 
-        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
-        {
-            if (task.IsCanceled)
-            {
-                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
-                return;
-            }
+        ErrorText.text = "";
+        
+        dbInstance.GetReference("users").GetValueAsync().ContinueWith(task => {
             if (task.IsFaulted)
             {
-                Debug.LogError("CreateUserWithEmailAndPasswordAsync error: " + task.Exception);
-                if (task.Exception.InnerExceptions.Count > 0)
-                    UpdateErrorMessage(task.Exception.InnerExceptions[0].Message);
-                return;
+                // Handle the error...
             }
-
-            FirebaseUser newUser = task.Result; // Firebase user has been created.
-            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
-                newUser.DisplayName, newUser.UserId);
-            UpdateErrorMessage("Signup Success");
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+                foreach (DataSnapshot user in snapshot.Children)
+                {
+                    IDictionary dictUser = (IDictionary)user.Value;
+                    if (dictUser["username"].ToString() == username)
+                    {
+                        return;
+                    }
+                    if (dictUser["email"].ToString() == email)
+                    {
+                        return;
+                    }
+                }
+            }
         });
+        
+
+        User new_user = new User(username, email, password, 0, 0);
+        string json = JsonUtility.ToJson(new_user);
+        reference.Child("users").Push().SetRawJsonValueAsync(json);
+        ErrorText.text = "User Created";
+
+        SceneManager.LoadScene("Main Menu");
+
     }
 
     private void UpdateErrorMessage(string message)
@@ -103,27 +130,26 @@ public class UserLogin : MonoBehaviour
 
     public void Login(string username, string password)
     {
-        auth.SignInWithEmailAndPasswordAsync(username, password).ContinueWith(task =>
-        {
-            if (task.IsCanceled)
-            {
-                Debug.LogError("SignInWithEmailAndPasswordAsync canceled.");
-                return;
-            }
+        dbInstance.GetReference("users").GetValueAsync().ContinueWith(task => {
             if (task.IsFaulted)
             {
-                Debug.LogError("SignInWithEmailAndPasswordAsync error: " + task.Exception);
-                if (task.Exception.InnerExceptions.Count > 0)
-                    UpdateErrorMessage(task.Exception.InnerExceptions[0].Message);
-                return;
+                // Handle the error...
             }
-
-            FirebaseUser user = task.Result;
-            Debug.LogFormat("User signed in successfully: {0} ({1})",
-                user.DisplayName, user.UserId);
-
-            PlayerPrefs.SetString("LoginUser", user != null ? user.Email : "Unknown");
-            //SceneManager.LoadScene("LoginResults");
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+                foreach (DataSnapshot user in snapshot.Children)
+                {
+                    IDictionary dictUser = (IDictionary)user.Value;
+                    if (dictUser["username"].ToString() == username && dictUser["password"].ToString() == password)
+                    {
+                        Debug.Log("" + user.Key + dictUser["username"] + " - " + dictUser["email"] + " - " + dictUser["password"]);
+                        SceneManager.LoadScene("Main Menu");
+                        break;
+                    }
+                }
+                
+            }
         });
     }
 
