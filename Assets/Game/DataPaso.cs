3d0c14e2e4e28f4dbe4c930e94c8dcb59c0c4d9b
FJJI <fjjimenez@miuandes.cl>
2020-10-26 07:20:34 -0300

    DataPaso tiene la Clase Nodo que es usada para ordenar la data de entrada y salida

--- Assets/Game/DataPaso.cs
+++ Assets/Game/DataPaso.cs
@@ -21,18 +21,43 @@ public class DataPaso : MonoBehaviour
         }
     }
 
-    // Ahora Aca dejaremos todo el codigo que queramos ir pasando entre escenas, este solo estara 1 vez, asi que debiese ser mas facil de tratar 
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class Nodo
     {
-        
-    }
+        public float posx;
+        public float posy;
+        public float posz;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public int type;
+        public int points;
+        public int total_nodes;
+        public int used_nodes;
+        public int owner;
+        public int healingFactor;
+        public int dmgFactor;
+        public int identifier;
+        public List<int> objectives;
+        public Nodo(GameObject nodo, int identifier)
+        {
+            this.identifier = identifier;
+
+            this.posx = nodo.transform.position.x;
+            this.posy = nodo.transform.position.y;
+            this.posz = nodo.transform.position.z;
+
+            Seleccion_y_Union data = nodo.GetComponent<Seleccion_y_Union>();
+            this.type = data.type;
+            this.points = data.points;
+            this.total_nodes = data.total_nodes;
+            this.used_nodes = data.used_nodes;
+            this.owner = data.owner;
+            this.healingFactor = data.healingFactor;
+            this.dmgFactor = data.dmgFactor;
+
+            objectives = new List<int>();
+        }
+        public void addObj(int data)
+        {
+            objectives.Add(data);
+        }
     }
 }
